f7a1b281a5dd4b21fdf3776491ac2fe07d2a7e43
이유진 <eujjin@naver.com>
2021-05-27 09:18:06 +0900

    cover_change

--- client/src/views/Cover.vue
+++ client/src/views/Cover.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="jb-box">
       <video muted autoplay loop>
-        <source src="@/assets/Cover.mp4" type="video/mp4">
+        <source src="@/assets/HomeCover.mp4" type="video/mp4">
         <strong>Your browser does not support the video tag.</strong>
       </video>
       <div class="jb-text">
